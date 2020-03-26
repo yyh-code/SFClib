af41906e72b2728a561e0097239186d01946b216
yangyanhao <yangyanhao@whu.edu.cn>
2020-03-26 14:14:37 +0800

    一维数组存放

--- SFC/kernel.cu
+++ SFC/kernel.cu
@@ -10,17 +10,18 @@
 //using namespace std;
 //#define dimension_size 32
 
-__global__ void add(unsigned int ** z,int a,int c, int b,int d)
+__global__ void add(unsigned int * z,int a,int c, int b,int d,int M)
 {
 	unsigned short pre_row = threadIdx.x + blockIdx.x * blockDim.x;
 	unsigned short pre_col = threadIdx.y + blockIdx.y * blockDim.y;
-	z[pre_row][pre_col] = 0;//一开始忘记给z[]赋初值！！！
+	z[M*pre_row+pre_col] = 0;//一开始忘记给z[]赋初值！！！
 	unsigned short row = pre_row + c;
 	unsigned short col = pre_col + b;
 	//printf("%d,%d\n", pre_row, pre_col);
 	if (row >=c && row <= a && col >= b && col <= d) {
+		
 		for (int i = 0; i < sizeof(row) * CHAR_BIT; i++) {
-			z[pre_row][pre_col] |= (row & 1U << i) << (i + 1) | (col & 1U << i) << i;
+			z[M*pre_row+pre_col] |= (row & 1U << i) << (i + 1) | (col & 1U << i) << i;
 		}
 	}
 }
@@ -80,36 +81,28 @@ void query()
 	clock_t start_time = clock();
 	int N = a - c + 1;
 	int M = d - b + 1;
-	//int number = N * M;
-	//int nBytes = number*sizeof(unsigned int);
-	unsigned int **z;
-	unsigned int *h_c;
-	unsigned int **d_z;
-	unsigned int *d_c;
-	cudaMalloc((void**)&d_z, N * sizeof(unsigned int*));
-	cudaMalloc((void**)&d_c, N * M * sizeof(unsigned int));
-	z = (unsigned int**)malloc(N * sizeof(unsigned int*));
-	h_c = (unsigned int*)malloc(N*M * sizeof(unsigned int));
-	for (int r = 0; r < N; r++)
-	{
-		z[r] = d_c + r*M;
-	}
-
-
-	cudaMemcpy((void*)d_z, (void*)z, N * sizeof(unsigned int*), cudaMemcpyHostToDevice);
-	int BLOCKCOLS = 1;
-	int BLOCKROWS = 1;
+	int number = N * M;
+	int nBytes = number*sizeof(unsigned int);
+	unsigned int *z;
+	unsigned int *d_z;
+	cudaMalloc((void**)&d_z, nBytes);
+	z = (unsigned int*)malloc(nBytes);
+	cudaMemcpy((void*)d_z, (void*)z, nBytes, cudaMemcpyHostToDevice);
+	int BLOCKCOLS = 16;
+	int BLOCKROWS = 16;
 	int gridCols = (M + BLOCKCOLS - 1) / BLOCKCOLS;
 	int gridRows = (N + BLOCKROWS - 1) / BLOCKROWS;
-	dim3 gridSize(gridCols, gridRows);
-	dim3 blockSize(BLOCKCOLS, BLOCKROWS);
-
+	dim3 gridSize(gridRows,gridCols);
+	dim3 blockSize(BLOCKROWS, BLOCKCOLS);
+	//dim3 blockSize(256);
+	
 	//dim3 blockSize(1,16);
 	//dim3 gridSize((number + blockSize.x*blockSize.y - 1) / (blockSize.x*blockSize.y));
 	//add << <gridSize, blockSize >> >(d_z);
-	add << <gridSize, blockSize >> >(d_z, a, c, b, d);
+	add << <gridSize, blockSize >> >(d_z, a, c, b, d,M);
+	//printf("$$$$$$$$$$$$$$$$$");
 	//add << <1, blockSize >> >(d_z, a, c, b, d);
-	cudaMemcpy((void*)h_c, (void*)d_c, N*M * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaMemcpy((void*)z, (void*)d_z, N*M * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	FILE *outfile;
 	outfile = fopen("SFC_z.txt", "w");
 	if (outfile == NULL) {
@@ -119,7 +112,7 @@ void query()
 	{
 		for (int col = 0; col < M; col++)
 		{
-			fprintf(outfile, "%d ", h_c[row*M + col]);
+			fprintf(outfile, "%d ", z[M*row + col]);
 		}
 		fprintf(outfile, "\n");
 	}
@@ -129,12 +122,11 @@ void query()
 	float clockTime = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC * 1000;
 	printf("Running time is:   %3.2f ms\n", clockTime);
 	cudaFree(d_z);
-	cudaFree(d_c);
 	free(z);
-	free(h_c);
 }
 int main()
 {
+	query();
 	int option;
 	while (1)
 	{
